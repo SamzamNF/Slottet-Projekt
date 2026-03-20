9be99daa6388397ac635f8204e03b618fa94ff39
SamzamNF <kongenakanico@gmail.com>
2026-03-20 16:53:36 +0100

    Add DbContext configuration for EFCore with SQL Server connection

--- Slottet csharp/Slottet.API/Program.cs
+++ Slottet csharp/Slottet.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Slottet.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the DI container.
@@ -6,12 +9,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // EF DbContext
 
+builder.Services.AddDbContext<EFContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+
 // Implementations from Infrastructure based on their interface in Application layer
 
 // Application services (Business logic)
 
 
 builder.Services.AddControllers();
+
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
