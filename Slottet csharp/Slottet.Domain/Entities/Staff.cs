515215e32f05d20f6050ed95b3cc916803f21138
Freja <fbfl71372@edu.ucl.dk>
2026-04-09 10:56:34 +0200

    Email er valgfri

--- Slottet csharp/Slottet.Domain/Entities/Staff.cs
+++ Slottet csharp/Slottet.Domain/Entities/Staff.cs
@@ -4,17 +4,13 @@ namespace Slottet.Domain.Entities;
 
 public class Staff
 {
-    public int Initials { get; set; } // Primary key
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string Email { get; set; } = string.Empty; // If�lge de nye svar
+    public int Id { get; set; }
+    public string Initials { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string? Email { get; set; }
+
+    // Navigation properties/foreign keys
     public int DepartmentId { get; set; }
     public string? Role { get; set; }
-
-    
-
-    /*public Department Department { get; set; }
-    public List<Phone> PhoneNumbers { get; set; }
-    public List<ResponsibilityArea> ResponsibilityAreas { get; set; }
-    */
 }
