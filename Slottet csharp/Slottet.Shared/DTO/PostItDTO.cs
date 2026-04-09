331a3338849ccaf608b3ca734f08196ead129bf4
Freja <fbfl71372@edu.ucl.dk>
2026-04-09 10:27:10 +0200

    DTO oprettet

---
+++ Slottet csharp/Slottet.Shared/DTO/PostItDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slottet.Shared.DTO
+{
+    internal class PostItDTO
+    {
+        public DateTime Date { get; set; }
+        public string Payment { get; set; } = string.Empty;
+        public string ShoppingDay { get; set; } = string.Empty;
+        public string Mood { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Events { get; set; } = string.Empty;
+        public string RelativesContact { get; set; } = string.Empty;
+    }
+}
