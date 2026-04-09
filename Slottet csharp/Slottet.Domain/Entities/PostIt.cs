b22090c31eb71a22523247023a4dd773e1d67113
Freja <fbfl71372@edu.ucl.dk>
2026-04-09 10:49:25 +0200

    Opdateret entities. Jeg har tilføjet "string.Empty" i stedet for "string?", da felterne altid findes i en post-it

--- Slottet csharp/Slottet.Domain/Entities/PostIt.cs
+++ Slottet csharp/Slottet.Domain/Entities/PostIt.cs
@@ -8,23 +8,16 @@ namespace Slottet.Domain.Entities
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
-        public string? Payment { get; set; }
-        public string? DayOfAction { get; set; }
-        public string? Status { get; set; }
-        public string? Mood { get; set; }
+        public string Payment { get; set; } = string.Empty;
+        public string ShoppingDay { get; set; } = string.Empty;
+        public string Mood { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Events { get; set; } = string.Empty;
+        public string RelativesContact { get; set; } = string.Empty;
 
         public int StaffId { get; set; }
         public int MedicineId { get; set; }
         public int ResidentId { get; set; }
         public int RiskId { get; set; }
-        
-
-
-        /*public Staff Staff { get; set; }
-        public Medicine Medicine { get; set; }
-        public Resident Residents { get; set; }
-        public Risk Risks { get; set; }
-        */
-
     }
 }
