cb22e3f9ff602d77b28e1c35e0a6d144f02f90de
Freja <fbfl71372@edu.ucl.dk>
2026-04-28 00:34:28 +0200

    Implementeret de 2 metoder for historik og tilføjet MapToDTO

--- Slottet csharp/Slottet.Application/BusinessLogic/PostItService.cs
+++ Slottet csharp/Slottet.Application/BusinessLogic/PostItService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using Slottet.Application.Interfaces;
+﻿using Slottet.Application.Interfaces;
 using Slottet.Domain.Entities;
 using Slottet.Shared.DTO;
 
@@ -49,6 +45,39 @@ public class PostItService
             Status = updatedPostIt.Status,
             Events = updatedPostIt.Events,
             RelativesContact = updatedPostIt.RelativesContact
+        };
+    }
+
+    public async Task<List<PostItDTO>> GetHistory(DateTime date)
+    {
+        // Call repository
+        var dateHistory = await _postItRepo.GetByDate(date);
+
+        // Return list of DTOs based on list of domain objects
+        return dateHistory.Select(postIt => MapToDTO(postIt)).ToList();
+    }
+    
+    public async Task<List<PostItDTO>> GetAllHistory()
+    {
+        // Call repository
+        List<PostIt> postIts = await _postItRepo.GetAll();
+
+        // Return list of DTOs based on list of domain objects
+        return postIts.Select(postIt => MapToDTO(postIt)).ToList();
+    }
+
+    // Helper method to map PostIt domain object to PostItDTO
+    private PostItDTO MapToDTO(PostIt postIt)
+    {
+        return new PostItDTO
+        {
+            Date = postIt.Date,
+            Payment = postIt.Payment,
+            ShoppingDay = postIt.ShoppingDay,
+            Mood = postIt.Mood,
+            Status = postIt.Status,
+            Events = postIt.Events,
+            RelativesContact = postIt.RelativesContact
         };
     }
 }
