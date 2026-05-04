b58cc056c03b1a708ca9688c8ee3f861c5a76c13
SamzamNF <kongenakanico@gmail.com>
2026-05-04 15:04:42 +0200

    Implement Phone management functionality with CRUD operations and role-based access

--- Slottet csharp/Slottet.Domain/Entities/Phone.cs
+++ Slottet csharp/Slottet.Domain/Entities/Phone.cs
@@ -7,7 +7,33 @@ namespace Slottet.Domain.Entities;
 public class Phone
 {
     public int Id { get; set; } // Primary key
-    public int PhoneNumber { get; set; } // Primary key
-    //public string Initials { get; set; } // Ikke sikker
+    public int PhoneNumber { get; set; } 
+
+    private Phone() { }
+
+    public static Phone Create(int phoneNumber)
+    {
+        // Sat requirement to between 8 and 14, can be changed to specifically 8 for denmark if needed
+        if (phoneNumber.ToString().Length < 8 || phoneNumber.ToString().Length > 14 )
+        {
+            throw new ArgumentException("Dit telefonnummer skal være mellem 8 og 14 tegn.");
+        }
+
+        return new Phone
+        {
+            PhoneNumber = phoneNumber
+        };
+    }
+
+    public void Update(int phoneNumber)
+    {
+        // Sat requirement to between 8 and 14, can be changed to specifically 8 for denmark if needed
+        if (phoneNumber.ToString().Length < 8 || phoneNumber.ToString().Length > 14 )
+        {
+            throw new ArgumentException("Dit telefonnummer skal være mellem 8 og 14 tegn.");
+        }
+
+        PhoneNumber = phoneNumber;
+    }
 
 }
---
+++ Slottet csharp/Slottet.Shared/DTO/PhoneDTO.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Slottet.Shared.DTO;
+
+public class PhoneDTO
+{
+    public int Id { get; set; }
+    public int PhoneNumber { get; set; }
+    
+}
