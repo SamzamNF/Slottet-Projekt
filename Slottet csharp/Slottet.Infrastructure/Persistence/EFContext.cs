f32dfdb426be7e23fe3dcb54d19bf394eb00940a
SamzamNF <kongenakanico@gmail.com>
2026-05-08 17:07:53 +0200

    Implement PnTime management with CRUD operations and repository pattern

--- Slottet csharp/Slottet.Infrastructure/Persistence/EFContext.cs
+++ Slottet csharp/Slottet.Infrastructure/Persistence/EFContext.cs
@@ -19,6 +19,7 @@ public class EFContext : DbContext, IUnitOfWork
     public DbSet<Department> Departments { get; set; } = null!;
     public DbSet<ResponsibilityArea> ResponsibilityAreas { get; set; } = null!;
     public DbSet<Phone> Phones { get; set; } = null!;
+    public DbSet<PnTime> PnTimes { get; set; } = null!;
 
     // Implementing IUnitOfWork, which saves changes as a transaction to the database
     public async Task<int> SaveChangesAsync()
